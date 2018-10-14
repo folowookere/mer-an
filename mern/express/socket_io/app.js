8e8448c4590968f7f15ee532d43b47909a08e7d6
Funmi <folowookere@yahoo.com>
2018-10-13 21:28:30 -0700

    Connection to socket.io is functioning

--- mern/express/socket_io/app.js
+++ mern/express/socket_io/app.js
@@ -5,11 +5,19 @@ var io = require('socket.io')(server);
 
 io.on('connection', function(client) { // connection event
     console.log('Client connected...');
+    client.on('messages', function(data) {
+        console.log(data);
+    });
+    // client.emit('messages', { hello: 'world'});
+    });
+        // client.on("messages", function(data) {
+        //     console.log(data);
+        //     client.emit("thread", data);
+        //     client.broadcast.emit("thread", data);
+        //   });
+        app.get('/', function(req, res) {
+        res.sendFile(__dirname + "/index.html");
+  });
 
-    client.emit('messages', {hello: 'world'}); //emit the 'message' event on the client
-});
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-server.listen(5500);
+//   app.use(express.static("/"));
+        server.listen(8080);
